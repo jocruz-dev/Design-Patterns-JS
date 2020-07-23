e882937d932b0be8e91b0c745f586248a41921ee
joshuaPCruz <alwaisjosh@hotmail.com>
2020-07-22 21:09:18 -0500

    Sixth Pattern Adding the first Structural pattern, the Proxy pattern

---
+++ Structural/proxy.js
@@ -0,0 +1,8 @@
+/**
+ * EN:
+ * This pattern, is focused on being a componet that optimize the calls 
+ * to an object, through a cache creation
+ * ES:
+ * Este patron, se enfoca en ser un componente que optimice las llamadas
+ * a un objeto, a traves de la creacion de un cache
+ */
